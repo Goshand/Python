518c153bd7e516822a67359d668302be4d348958
Goshand <48172331+Goshand@users.noreply.github.com>
2021-02-05 12:14:27 -0800

    Update spaceminer.py

--- spaceminer.py
+++ spaceminer.py
@@ -1,23 +1,60 @@
 import os
-from pathlib import Path
 
-def output_dir(path, num_tabs):
-	gap=""
-	for i in range(num_tabs):
-		gap+="  "
-	size=0		
-	for filename in os.listdir(path):
-		full_path = path + "/" + filename
-		if os.path.isdir(full_path):
-			print (gap + "Directory: " + filename)
-			size+=output_dir(full_path, num_tabs+1)
-		elif os.path.isfile(full_path):
-			size+=os.path.getsize(full_path)
-			print (gap + "File: " + filename + " = " + str(os.path.getsize(full_path)))
-		else:
-			print(gap + "Other: " + filename)
-	print(gap + "Size of directory: " + path + " = " + str(size))
-	return size
+os.system("clear")
+
+class File:
+
+	def __init__(self, path, filename, size, num_tabs):
+		self.path = path
+		self.filename = filename
+		self.size = size
+		self.num_tabs = num_tabs
+		
+	def __str__(self):
+		gap=""
+		for i in range(self.num_tabs):
+			gap+="  "
+		return gap + self.filename + " - " + str(self.size)
+		
+	def print_me(self):
+		gap=""
+		for i in range(self.num_tabs):
+			gap+="  "
+		print(gap + self.filename + " - " + str(self.size))
+		
+	def print_out(self):
+		self.print_me()
+		
+class Directory(File, object):
+	def __init__(self, path, filename, size, num_tabs):
+		super(Directory, self).__init__(path, filename, size, num_tabs)
+		self.list_of_stuff=[]
 	
-path=input("What path do you want to start with?")
-output_dir(path,0)
+	def addToStuff(self, thing):
+		self.list_of_stuff+=[thing]
+	
+	def addToSize(self, size):
+		self.size += size
+		
+	def print_out(self):
+		self.print_me()
+		for thing in self.list_of_stuff:
+			thing.print_out()
+
+def output_dir(directory, num_tabs):
+	for filename in os.listdir(directory.path):
+		full_path = directory.path + "/" + filename
+		if os.path.isdir(full_path):
+			new_dir = Directory(full_path, filename, 0, num_tabs)
+			directory.addToStuff(new_dir)
+			output_dir(new_dir, num_tabs+1)
+			directory.addToSize(new_dir.size)
+		elif os.path.isfile(full_path):
+			file_size = os.path.getsize(full_path)
+			new_file = File(full_path, filename,file_size, num_tabs)
+			directory.addToStuff(new_file)
+			directory.addToSize(file_size)
+			
+path=input("What path do you want to start with: ")
+start_dir = Directory(path, path, 0, 0)
+output_dir(start_dir,1)
